7c4f4fc5f35a99b4c47024570cf1792d9f9c412e
clockhunter-muqri <137082784+clockhunter-muqri@users.noreply.github.com>
2025-12-21 07:56:48 +0800

    Update rsp.launch.py

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -17,7 +17,9 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('amr_robot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+
+    #Robot Desciption Parameter
+    
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
